b45b05dc43a037651846171e6b1309c7b290a600
gabrielestevaodossantossilva-os <gabriel.estevaodossantos.silva@escola.pr.gov.br>
2026-09-02 00:18:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-// Contador dos botões.
+// Botões do primeiro post
+let coracao1 = 0;
+let joinha1 = 0;
 
-let coracao = 0;
-let joinha = 0;
-
-function curtirCoracao() {
-    coracao++;
-    document.getElementById("coracao").innerHTML = coracao;
-    document.getElementById("coracao2").innerHTML = coracao;
-    document.getElementById("coracao3").innerHTML = coracao;
+function curtirCoracao1() {
+    coracao1++;
+    document.getElementById("coracao1").innerHTML = coracao1;
 }
 
-function curtirJoinha() {
-    joinha++;
-    document.getElementById("joinha").innerHTML = joinha;
-    document.getElementById("joinha2").innerHTML = joinha;
-    document.getElementById("joinha3").innerHTML = joinha;
+function curtirJoinha1() {
+    joinha1++;
+    document.getElementById("joinha1").innerHTML = joinha1;
+}
+
+// Botões do segundo post
+let coracao2 = 0;
+let joinha2 = 0;
+
+function curtirCoracao2() {
+    coracao2++;
+    document.getElementById("coracao2").innerHTML = coracao2;
+}
+
+function curtirJoinha2() {
+    joinha2++;
+    document.getElementById("joinha2").innerHTML = joinha2;
+}
+
+// Botões do terceiro post
+let coracao3 = 0;
+let joinha3 = 0;
+
+function curtirCoracao3() {
+    coracao3++;
+    document.getElementById("coracao3").innerHTML = coracao3;
+}
+
+function curtirJoinha3() {
+    joinha3++;
+    document.getElementById("joinha3").innerHTML = joinha3;
 }
